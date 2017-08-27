29cb77d31a03420d133599745df762de311fea6b
Bob <negenentwintig@hotmail.com>
2017-08-27 10:50:16 +0200

    reassignment

--- try_all.sh
+++ try_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for h in {lmcut,blind,landmark,hadd,hff,hmax,hsa}; do
-    for s in {astar,wastar,gbf,bfs,ehs,ids}; do
-        echo $h
-        echo $s
-        ./src/pyperplan.py -H $h -s $s domain.pddl problem.pddl | grep expanded
-    done
+for h in {blind,hadd,hff,hmax}; do
+    echo astar: $h
+    ./src/pyperplan.py -H $h -s astar domain.pddl problem.pddl | grep expanded
 done
-
+echo bfs
+./src/pyperplan.py -s bfs domain.pddl problem.pddl | grep expanded
+echo ids
+./src/pyperplan.py -s ids domain.pddl problem.pddl | grep expanded
